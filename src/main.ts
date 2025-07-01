ac26f2e28529323d26e58d2b6e16c90d20df3c58
Henry Blanchette <henry@monkeyface.com>
2025-07-01 10:43:57 -0400

    [checkpoint]

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import filenamify from "filenamify";
-import { do_ } from "./utility";
+import { do_, stringify } from "./utility";
 import { GenerateMetadataOfPaperPdf } from "./ai";
 
 const config = {
@@ -22,25 +22,54 @@ const failed_lines: string[] = [];
 for (const line of input_lines) {
   console.log(`processing: ${line}`);
   if (!URL.canParse(line)) {
-    console.warn(`can't parse URL: ${line}`);
+    console.error(`can't parse URL: ${line}`);
     failed_lines.push(line);
     continue;
   }
   const url_parsed = URL.parse(line)!;
   const url = `${url_parsed.origin}${url_parsed.pathname}`;
   const tmp_filename = `${config.doc_dirpath}/${filenamify(url)}.pdf`;
-  const pdf_response = await Bun.fetch(url);
-  await Bun.write(tmp_filename, pdf_response);
 
-  // // TODO: use pdf-parse somehow on pdf_response
-  // // TODO: extract title
-  // // TODO: extract abstract
-  const metadata = await GenerateMetadataOfPaperPdf.Flow({
-    doc_filepath: tmp_filename,
-  });
+  if (!(await Bun.file(tmp_filename).exists())) {
+    const pdf_response = await do_(async () => {
+      try {
+        return await Bun.fetch(url);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.error(e.toString());
+          return undefined;
+        } else {
+          throw e;
+        }
+      }
+    });
+    if (pdf_response === undefined) continue;
 
-  // TODO: extract references
-  // TODO: generate overview
+    await Bun.write(tmp_filename, pdf_response);
+  }
+
+  try {
+    // // TODO: use pdf-parse somehow on pdf_response
+    // // TODO: extract title
+    // // TODO: extract abstract
+    const metadata = await GenerateMetadataOfPaperPdf.Flow({
+      doc_filepath: tmp_filename,
+    });
+
+    console.log(stringify(metadata));
+
+    // TODO: extract references
+    // TODO: generate overview
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error(e.toString());
+    } else {
+      throw e;
+    }
+    continue;
+  }
+
+  process.exit(0);
 }
 
 // TODO: write failed_lines back to original file
